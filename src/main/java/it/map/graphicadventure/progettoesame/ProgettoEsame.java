68a777686d4085c317aae5ee2c315a83dfd03366
Davide Sicolo <d.sicolo4@studenti.uniba.it>
2026-06-16 12:32:58 +0200

    First commit

---
+++ src/main/java/it/map/graphicadventure/progettoesame/ProgettoEsame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.map.graphicadventure.progettoesame;
+
+/**
+ *
+ * @author David
+ */
+public class ProgettoEsame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
