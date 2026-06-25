fdc05ca196ac2865e7247598b0ead42bfa296b31
Davide Sicolo <d.sicolo4@studenti.uniba.it>
2026-06-25 15:56:30 +0200

    Aggiunto il MapBuilder da file e aggiunto file GameController

---
+++ src/main/java/it/map/graphicadventure/progettoesame/MapBuilder.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.map.graphicadventure.progettoesame;
+
+/**
+ *
+ * @author David
+ */
+
+import it.map.graphicadventure.progettoesame.GameUtils;
+import it.map.graphicadventure.progettoesame.type.Room;
+import java.util.List;
+
+public class MapBuilder {
+
+    public Room buildWorld() {
+        
+        try {
+            // Leggiamo la mappa dal file esterno usando la tua GameUtils!
+            List<Room> stanze = GameUtils.loadMapFromFile("src/main/resources/mappa.txt");
+            
+            // Assumiamo che la prima stanza della lista (indice 0) sia quella iniziale
+            return stanze.get(0); 
+            
+        } catch (Exception e) {
+            System.out.println("Errore fatale nel caricamento della mappa: " + e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ src/main/java/it/map/graphicadventure/progettoesame/controller/GameController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.map.graphicadventure.progettoesame.controller;
+
+/**
+ *
+ * @author David
+ */
+public class GameController {
+    
+}
